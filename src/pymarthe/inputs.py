935cc22fe02a3d344df338a3fbb8d6d846a9c882
Vergnes Jean-Pierre <JP.Vergnes@brgm.fr>
2021-11-09 22:26:59 +0100

    Inputs/outputs code for Marthe

--- src/pymarthe/inputs.py
+++ src/pymarthe/inputs.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def build_entete(yentete):
     lines = [yentete]
     lines.append(
@@ -26,3 +28,188 @@ def build_line_xy(gig, champ, x, y, cou, name=''):
         "   /XCOO:X={2:7.0f}Y={3:7.0f}P={4:7d};{5}".format(
             str(gig), champ, x, y, cou, name))
 
+def build_histo_collig(df):
+    lines = []
+    for row in df:
+        if row['GIG'] == 0:
+            row['GIG'] = ''
+        lines.append(
+            build_line_collig(row['GIG'], row['CHAMPS'], row['COL'],
+                    row['LIG'], row['COU'], row['NAME'])
+                    )
+    return lines
+
+def set_file_histo_collig(filename, yentete, df):
+    lines = [] 
+    lines.extend(build_entete(yentete))
+    lines.extend(build_histo_collig(df))
+    with open(filename, 'w', encoding='cp1252') as f:
+        f.write('\n'.join(lines))
+
+def build_timestep_line(var, typ, **kwargs):
+    """
+    Edit a line of a Marthe time step file 
+
+    Parameters
+    ----------
+    var : str
+        Variable to edit. Possible values are all the variable of
+        MARTHE
+    typ : str
+        Geometry. Possible values are 
+            'EDITION', 'COUCHE', 'ZONE_GEO', 'ZONEP', 'MAILLE', 'TRONCON', 'LIST_MAIL'
+    
+    Return
+    ------
+    line : str
+        Line formatted for the MARTHE file
+    """
+    doptions = {
+        'GRILLE':'N: {N}',
+        'LIST_MAIL':'N: {N}',
+        'COUCHE':'C={C:>7d}V={V:>10};',
+        'ZONE_GEO':'Z={Z:>7d}V={V:>10};',
+        'MAILLE':'C={C:>7d}L={L:>7d}P={P:>7d}V={V:>10};',
+        'FICH_METE':'N={N:>7}',
+        'ZONEP':'Z={Z:>7}V={V:>10};',
+        'EDITION':'I={I:>2};',
+        'FICHIER':'N: {N:>7}',
+        'TRONCON':'A={A:>7}T={T:>7}V={V:>10};',
+    }
+    deditions = {
+        'VITESSE':'I={I:>2} [C={C:>7d};L={L:>7d};P={P:>7d};Gig={Gig:>2d}]',
+        'FLUX_INFILTR':'I={I:>2};Z={Z:>2};C={C:>2};',
+        'DEBIT':'*={star:>2};V={I:>2};L={L:>2};S={F:>2};Z={Z:>2};',
+        'DEBIT_RIVI':'I={I:>2};L={L:>2};F={F:>2};B={B:>2};',
+        'RUISSEL':'I={I:>2};Z={Z:>2};C={C:>2};',
+        'RECHARGE':'I={I:>2};Z={Z:>2};C={C:>2};',
+        'FLUX_PLUV':'I={I:>2};Z={Z:>2};C={C:>2};',
+        'PROFOND_NAPP':'I={I:>2};Z={S:>2};',
+        'HAUTEU_RIVI':'I={I:>2};P={P:>2};',
+        'SATUR_NAQ':'I={I:>2};R={P:>2};',
+    }
+    list_mail_opt = ['<Somm_Mail>', '<Keep_9999>', '<X_Y_V>',
+                    '<Moyenne>', '<Init_a_Zero>', '<X_Y_C>', '<X_Y_Z>']
+    yvar_type = '/{0}/{1}'.format(var, typ)
+    yfile = kwargs.get('File', '')
+    if yfile:
+        yfile =' File= {0}'.format(kwargs['File'])
+    ycolumn = kwargs.get('column', '')
+    if ycolumn and int(ycolumn) == 1:
+        ycolumn = ''
+    elif ycolumn and int(ycolumn) > 1:
+        ycolumn = '; Col={0}'.format(ycolumn)
+    if typ == 'EDITION':
+        opts = ['star', 'V', 'L', 'S', 'Z', 'I', 'F', 'B', 'C', 'P', 'Gig']
+        dtemp = {opt:kwargs.get(opt, ' ') for opt in opts}
+        if var in deditions.keys():
+            dopt = deditions[var]
+        else:
+            dopt = doptions[typ]
+        yoptions = dopt.format(**dtemp)
+        yoptions = '{0}{1}'.format(yoptions, yfile)
+    else:
+        yoptions = doptions[typ].format(**kwargs)
+        if typ == 'LIST_MAIL':
+            opts = kwargs.get('list_mail_opts', [])
+            opts = [opt for opt in opts if opt in list_mail_opt]
+            yoptions = '{0} {1}'.format(yoptions, ' '.join(opts))
+        if typ in ['COUCHE', 'ZONE_GEO', 'ZONEP', 'MAILLE', 'TRONCON']:
+            yoptions = '{0}{1}{2}'.format(yoptions, yfile, ycolumn)
+    return '  {0:25}{1}'.format(yvar_type, yoptions)
+
+def build_init_timestep(date_perm, length, dactions):
+    """
+    Build the lines for initialization time step of MARTHE pastp file
+
+    Parameters
+    ----------
+    date_perm : str
+        Format %Y/%m/%d
+    length : int
+    dactions : dict
+
+    Return
+    ------
+    lines : list of str
+    """
+    ystart_sim = ' *** Début de la simulation    à la date :{fdate}; ***'
+    lines = [ystart_sim.format(fdate=' {0:{1}} '.format(date_perm, length))]
+    actions = dactions.get(date_perm, [''])
+    for action in actions:
+        lines.append(action)
+    lines.append('  /*****/***** Fin de ce pas')
+    return lines
+
+def build_pastp(dates_trans, length, dactions):
+    """
+    Build all lines of a pastp file
+
+    Parameters
+    ----------
+    dates_trans : list of str
+    length : int
+    dactions : dict
+
+    Return
+    ------
+    list of str 
+    """
+    lines = []
+    ystart_step = ' *** Le pas :{fpas:5d}: se termine à la date :{fdate}; ***'
+    for i, date in enumerate(dates_trans):
+        lines.append(ystart_step.format(
+            fpas=i+1, fdate=' {0:{1}} '.format(date, length)))
+        actions = dactions.get(date, [])
+        for action in actions:
+            if action:
+                lines.append(action)
+        lines.append('  /*****/***** Fin de ce pas')
+    return lines
+
+def set_file_pastp(filename, yentete, date_perm, dates_trans, dactions={}):
+    """
+    Build a pastp file
+
+    Parameters
+    ----------
+    filename : str
+        name of the pastp file
+    yentete : str
+        first lines of the pastp file
+    date_perm : str
+    date_trans : list of str
+    dactions : dict
+        keys are dates in date_perm and date_trans
+    """
+    length = max([len(str(date_perm))] + [len(str(date)) for date in dates_trans])
+    lines = [] 
+    lines.extend(build_entete(yentete))
+    lines.extend(build_init_timestep(date_perm, length, dactions))
+    lines.extend(build_pastp(dates_trans, length, dactions))
+    lines.append(' ***        :     : Fin de la simulation :            ; ***')
+    with open(filename, 'w') as f:
+        f.write('\n'.join(lines))
+
+def set_edit_file(filename, entete, start, end, dates):
+    """
+    Build an editions file
+
+    Parameters
+    ----------
+    filename : str
+        name of the pastp file
+    entete : str
+        first lines of the pastp file
+    start : datetime.datetime
+    end : datetime.datetime
+    dates : list of datetime.datetime
+        date for which an edition will be set
+    """
+    with open(filename, 'w') as f:
+        f.write('{0}\n'.format(entete))
+        for date in pd.date_range(start, end):
+            if date in dates:
+                f.write('1\t{0}\n'.format(date.strftime("%Y/%m/%d")))
+            else:
+                f.write('0\t{0}\n'.format(date.strftime("%Y/%m/%d")))
--- src/pymarthe/outputs.py
+++ src/pymarthe/outputs.py
@@ -2,11 +2,25 @@
 Read outputs
 """
 import datetime
+from io import StringIO
+import os
 import re
+import shutil
+
+import numpy as np
 import pandas as pd
+import pylab as plt
 
 def date_parser(x):
     return datetime.datetime.strptime(x, '%d/%m/%Y')
+
+def gen_glob(in_file):
+    with open(in_file, encoding='ISO-8859-1') as f:
+        for line in f:
+            if line.strip():
+                yield(line)
+            else:
+                break
 
 def open_historiq(fname):
     """
@@ -39,3 +53,264 @@ def open_historiq(fname):
     df.columns.names = ['Field', 'XY', 'Label']
     df.index.name = 'Date'
     return df
+
+def open_histoclim(fname, **kwargs):
+    """
+    Read a "histoclim.prn" file of MARTHE
+
+    Parameters
+    ----------
+    fname : str
+
+    Returns
+    -------
+    DataFrame
+        DataFrame containing the values of the input file fname
+    """
+    def chunk(s, n):
+        for start in range(0, len(s), n):
+            yield s[start:start+n]
+    f = StringIO(''.join([line for line in gen_glob(fname)]), newline='\n')
+    df = pd.read_csv(
+        f,
+        delim_whitespace=True,
+        skiprows=3,
+        index_col=0,
+        header=None,
+        engine='python',
+        parse_dates=True,
+        date_parser=date_parser,
+        encoding='ISO-8859-1',
+        **kwargs
+    )
+    with open(fname, encoding='ISO-8859-1') as f:
+        for _ in range(3):
+            line = f.readline()
+    df.columns = [c.strip() for c in chunk(line.strip(), 16)][1:]
+    return df
+
+def open_histobil_debit(fname, **kwargs):
+    """
+    Read a "histobil_debit.prn" file of MARTHE
+    If zone budgets are presents, only the global budget is readed
+
+    Parameters
+    ----------
+    fname : str
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    f = StringIO(''.join([line for line in gen_glob(fname)]), newline='\n')
+    df = pd.read_csv(
+        f,
+        delim_whitespace=True,
+        skiprows=2,
+        index_col=0,
+        engine='python',
+        parse_dates=True,
+        date_parser=date_parser,
+        encoding='ISO-8859-1',
+        **kwargs
+    )
+    return df
+
+def mv_results(dir_mod,  dir_res):
+    """
+    Create a directory for a simulation for transfering outputs
+
+    Parameters
+    ----------
+    dir_mod : str
+        model directory
+    dir_res : str
+        results directory
+    """
+    if not os.path.isdir(dir_res):
+        os.makedirs(dir_res)
+    else:
+        ans = input('{0} already exists. Replace it ? (y/N)'.format(dir_res))
+        if ans.lower() in ['', 'n', 'no', 'non']:
+            return
+    filenames = [
+        'bilandeb.txt', 'chasim.out', 'debsim.out', 'histobil_debit.prn',
+        'histobil_nap_cumu.prn', 'histobil_nap_pastp.prn', 'histobil_riv_dra_lac.prn',
+        'histoclim.prn', 'historiq.out', 'historiq.prn', 'mart_ver.txt', 'marthe.txt',
+        'converg.txt', 'rivsim.prn'
+        ]
+    for filename in filenames:
+        mod_fichier = '{0}/{1}'.format(dir_mod, filename)
+        res_fichier = '{0}/{1}'.format(dir_res, filename)
+        if os.path.isfile(mod_fichier):
+            shutil.copy(mod_fichier, res_fichier)
+
+def concat_cloneades(dfsim, dir_obs, codes):
+    """
+    Concatenate a pandas.DataFrame from a .csv file of "clone ADES"
+    with a dataframe of a historiq.prn from a MARTHE simulation.
+
+    Only work with the "Charge" component
+
+    Parameters
+    ----------
+    dfsim : pandas.DataFrame
+        DataFrame of an historiq.prn
+    dir_obs : str
+        Name of the directories where lies the csv files of observations
+    codes : list of int
+        List of BSS code
+
+    Return
+    ------
+    pandas.DataFrame
+        Observation and simulation with codes BSS in columns 
+    """
+    dftot = []
+    col_sim = dfsim.columns.get_level_values('Label')
+    for code_bss in codes:
+        dfobs = pd.read_csv(
+            '{0}/{1}.csv'.format(dir_obs, code_bss.replace('/', '_')),
+            parse_dates=True, index_col='date_mesure', sep=';',
+            encoding='cp1252'
+            )
+        dfobs = dfobs['val_calc_ngf'].resample('D').mean()
+        code_sim = col_sim[col_sim.str.contains(code_bss.split('/')[0])]
+        assert len(code_sim) == 1
+        dftot.append(
+            build_obs_sim(
+                dfobs,
+                dfsim.loc[:, ('Charge', slice(None), code_sim)].squeeze(),
+                code_bss
+                )
+            )
+    return pd.concat(dftot, axis=1)
+
+def concat_bdhydro2(dfsim, dir_obs, codes):
+    """
+    Concatenate a pandas.DataFrame from a .csv file of "clone ADES"
+    with a dataframe of a historiq.prn from a MARTHE simulation.
+
+    Only work with the "Débit_Riv" component
+
+    Parameters
+    ----------
+    dfsim : pandas.DataFrame
+        DataFrame of an historiq.prn
+    dir_obs : str
+        Name of the directories where lies the csv files of observations
+    codes : list of int
+        List of BSS code
+
+    Return
+    ------
+    pandas.DataFrame
+        Observation and simulation with codes BSS in columns 
+    """
+    dftot = []
+    col_sim = dfsim.columns.get_level_values('Label')
+    for code in codes:
+        dfobs = pd.read_csv(
+            '{0}/{1}.csv'.format(dir_obs, code), parse_dates=True,
+            index_col='Date', sep=';', encoding='cp1252'
+            )
+        dfobs = dfobs.resample('D').mean()
+        dfobs = dfobs.replace(-2, np.nan)
+        dfobs = dfobs.dropna()
+        code_sim = col_sim[col_sim.str.contains(code)]
+        assert len(code_sim) == 1
+        dftot.append(
+            build_obs_sim(
+                dfobs.squeeze(),
+                dfsim.loc[:, ('Débit_Rivi', slice(None), code_sim)].squeeze(),
+                code
+                )
+            )
+    return pd.concat(dftot, axis=1)
+
+def build_obs_sim(dfobs, dfsim, code):
+    """
+    Return a dataframe two columns "obs" and "sim" for the specified BSS code.
+    Code needs to be available in dfobs and dfsim.
+
+    Parameters
+    ----------
+    dfobs : pandas.DataFrame
+    dfsim : pandas.DataFrame
+    code : str
+
+    Return
+    ------
+    pandas.DataFrame
+    """
+    assert len(dfobs.shape) == 1
+    assert len(dfsim.shape) == 1
+    df = pd.DataFrame(
+        {'obs':dfobs, 'sim':dfsim}
+    )
+    df.columns = pd.MultiIndex.from_product(
+        [[code], ['obs', 'sim']], names=['code', 'type']
+        )
+    return df
+
+def plot_head(dftot, dir_fig, xp, codes):
+    """
+    Create figures with observed and simulated piezometric head for the 
+    specific codes
+
+    Parameters
+    ----------
+    dftot : pandas.DataFrame
+        dftot is created with concat_cloneades
+    dir_fig : str
+        diretory in which figures go
+    xp : str
+        name to give to the simulation
+    codes : list of str
+    """
+    if not os.path.isdir(dir_fig):
+        os.makedirs(dir_fig)
+    for code in codes:
+        df = dftot.xs(level='code', key=code, axis=1)
+        ax = df.plot(grid=True, legend=True, style=['b', 'r--'])
+        ax.set_ylabel('m')
+        ax.set_title('Charge {0} {1}'.format(xp, code))
+        plt.savefig(
+            '{0}/charges_{1}_{2}.png'.format(
+                dir_fig, xp, code.replace('/', '_')),
+                dpi=300
+                )
+        plt.close('all')
+
+def plot_riverflow(dftot, dir_fig, xp, codes):
+    """
+    Create figures with observed and simulated river flows for the 
+    specific codes
+
+    Parameters
+    ----------
+    dftot : pandas.DataFrame
+        dftot is created with concat_bdhydro2
+    dir_fig : str
+        diretory in which figures go
+    xp : str
+        name to give to the simulation
+    codes : list of str
+    """
+    if not os.path.isdir(dir_fig):
+        os.makedirs(dir_fig)
+    for code in codes:
+        df = dftot.xs(level='code', key=code, axis=1)
+        ax = df.plot(grid=True, legend=True, style=['b', 'r--'])
+        ax.set_ylabel('m3/s')
+        ax.set_title('Débit Rivière {0} {1}'.format(xp, code))
+        plt.savefig(
+            '{0}/debits_rivi_{1}_{2}.png'.format(
+            dir_fig, xp, code.replace('/', '_')),
+            dpi=300
+            )
+        plt.close('all')
+
+
+
+
