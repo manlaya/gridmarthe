9c2c4da7c74e526b1e44ac5508db138f02fd7b9e
Adrien Manlay <a.manlay@brgm.fr>
2025-10-11 14:45:25 +0200

    fix plot oucrop colorbar/cmap, allow custom cmap

--- src/gridmarthe/plot.py
+++ src/gridmarthe/plot.py
@@ -22,6 +22,8 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+from copy import copy
+
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import (colors, cm)
@@ -56,13 +58,13 @@ def plot_nested_grid(ds, ax=None, varname='charge', **kwargs):
         the dataset MUST be a 2D array, with dims = x,y.
         In other words, you may need to sel z and time before plot, and you need to apply `assign_coord()`
     
-    ax: matplotlib axe, Optionnal.
+    ax: matplotlib axe, optional.
         if provided, data are plotted on this axis, otherwise fig, ax instances will be created.
     
-    varname: str, Optionnal
+    varname: str, optional
         the variable to plot in dataset. Default is 'charge'.
     
-    kwargs: optionnal.
+    kwargs: optional.
         any keywords argument from `xr.Dataset.plot.pcolormesh()`
     
     Returns
@@ -70,6 +72,7 @@ def plot_nested_grid(ds, ax=None, varname='charge', **kwargs):
     ax: matplotlib axis.
     """
     da = ds.copy()
+    
     if 'var' in kwargs.keys():
         # legacy, previous arg name was only var, harmonize between functions/methods
         varname = kwargs.pop('var')
@@ -102,6 +105,10 @@ def plot_nested_grid(ds, ax=None, varname='charge', **kwargs):
     
     _set_map_lims(ax, da.x.min().data, da.y.min().data, da.x.max().data, da.y.max().data)
     
+    if str(da.time.values)[:10] == '1850-01-01':
+        # remember: no slice on time because it must be selected before calling function
+        ax.set_title(varname)  # remove default title from xarray API if dummy timestep (e.g plot parameters)
+    
     return ax
 
 
@@ -117,13 +124,13 @@ def plot_mesh_time_serie(*arg, zone: int, varname='charge', show=False, figsize=
     zone: int
         zone value (dimension) to select data
     
-    varname: str, Optionnal (default is 'charge')
+    varname: str, optional (default is 'charge')
         Variable to plot. Must be a key of all dataset passed as *arg.
     
-    show: bool, Optionnal.
+    show: bool, optional.
         show plot using `plt.show()`
     
-    figsize: tuple[int], Optionnal.
+    figsize: tuple[int], optional.
         figsize argument for matplotlib.
     
     kwargs: any keywords argument for `xr.Dataset.plot()` method
@@ -142,57 +149,94 @@ def plot_mesh_time_serie(*arg, zone: int, varname='charge', show=False, figsize=
     return ax
 
 
-def plot_outcrop(ds_outcrop, file_out=None, engine='xr', show=False):
-    
+def plot_outcrop(ds_outcrop, fig=None, ax=None, cbar_width='3', labels=None, file_out=None, show=False, **kwargs):
     """ Usefull function to plot outcrop layers of a marthe (multilayer) grid
     
     There is two mode implementend yet, using xr.plot or
-    gpd.plot (useful for nested grid)
+    gpd.plot (useful for nested grid).
 
-    if engine == 'xr' => ds_outcrop need to get coords before
-        (use `gm.assign_coords(ds_outcrop, add_lay=False)`)
-    if engine == 'gpd => ds_outcrop need to be a gpd.GeoDataframe
-        (use `gm.to_geodataframe(ds_outcrop)`)
+    To use this function, `ds` provided need to have a `z` variable (i.e model
+    used need to be multilayer) and user need to assign coords before and **keep**
+    the z dimension as a variable (see Note).
     
-    nb: The Marthe grid must have a `z` dimension.
+    Note
+    ----
+    - if input is a `xr.Dataset` instance, `ds_outcrop` need to get coords before
+    but keep `z` as a variable (use `gm.assign_coords(ds_outcrop, add_lay=False)`)
     
     Parameters
     ----------
     ds_outcrop: xr.Dataset
         output of `gm.get_surface_mask()`
     
-    file_out: str, Optionnal.
+    fig: matplotlib figure, Optional.
+        if provided, data are plotted on this figure, otherwise fig, ax instances will be created
+    
+    ax: matplotlib axe, Optional.
+        if provided, data are plotted on this axis, otherwise fig, ax instances will be created
+    
+    cbar_width: str, Optional.
+        width of colorbar, as a percentage of the main axis. Default is '3'
+    
+    labels: list, Optional.
+        labels for layers in colorbar
+
+    file_out: str, Optional.
         If not None (default), file name to write plot
     
-    engine: str, Optionnal.
-        Engine to use for plotting (default is `xr`).
-    
-    show: bool, Optionnal.
+    show: bool, Optional.
         Show result (`plt.show()`), default is False.
+    
+    kwargs: dict, Optional.
+        Any keywords argument to pass to either `xarray.Dataset.plot.pcolormesh()`
+        or `geopandas.GeoDataFrame.plot()`.
     
     Returns
     -------
     fig, ax, ax_cbar if not `show`, otherwise return None
+    
+    Example
+    -------
+    xarray version:
+    >>> ds_surf = gm.get_surface_layer(ds)
+    >>> gm.plot_outcrop(gm.assign_coords(ds_surf, add_lay=False))
+
+    geopandas version:
+    >>> ds_surf = gm.get_surface_layer(ds)
+    >>> gm.plot_outcrop(gm.to_geodataframe(ds_surf))
     """
     if isinstance(ds_outcrop, xr.Dataset):
         assert 'z' in ds_outcrop.keys(), "No `z` dimension. Outcrop plot is not possible."
         maxn = np.nanmax(ds_outcrop['z'].data)
+        engine = 'xr'
     elif isinstance(ds_outcrop, gpd.geodataframe.GeoDataFrame):
         assert 'z' in ds_outcrop.columns, "No `z` dimension. Outcrop plot is not possible."
         maxn = np.nanmax(ds_outcrop['z'].to_numpy())
+        engine = 'gpd'
 
     # custom cbar to force categories
-    cmap = cm.tab10 if maxn <= 10 else cm.tab20
-    cmap = colors.ListedColormap(cmap.colors[:int(maxn)]) # subset on number of colors, if not wrong legend
-    bounds = np.arange(1, maxn+1)
-    if len(bounds) == 1:
-        bounds = np.append(bounds, [maxn+1])
-    norm = colors.BoundaryNorm(bounds, cmap.N+1,)  # set bins to custom values
-    cbar = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+    cmap = kwargs.pop('cmap', None)
+    norm = kwargs.pop('norm', None)
     
-    fig, ax = plt.subplots(1, 1)
+    if cmap is None:
+        cmap = cm.tab10 if maxn <= 10 else cm.tab20
+        cmap = colors.ListedColormap(cmap.colors[:int(maxn)])  # subset on number of colors, if not wrong legend
+    
+    if norm is not None:
+        bounds = norm.boundaries
+    else:
+        bounds = np.arange(1, maxn+2)
+        if len(bounds) == 1:
+            bounds = np.append(bounds, [maxn+1])
+        norm = colors.BoundaryNorm(bounds, cmap.N)  # set bins to custom values
+    
+    sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+    
+    # Create the figure and axes, with a place for colorbar
+    if fig is None or ax is None:
+        fig, ax = plt.subplots(1, 1)
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.1)
+    cax = divider.append_axes("right", size="{}%".format(cbar_width), pad=0.1)
     
     if engine == 'xr':
         the_plot = plot_nested_grid(
@@ -201,6 +245,7 @@ def plot_outcrop(ds_outcrop, file_out=None, engine='xr', show=False):
             cmap=cmap,
             levels=bounds,
             add_colorbar=False,
+            **kwargs
         )
     
     elif engine == 'gpd':
@@ -212,16 +257,24 @@ def plot_outcrop(ds_outcrop, file_out=None, engine='xr', show=False):
             cmap=cmap,
             legend=False,
             norm=norm,
+            **kwargs
         )
     
     ax_cbar = fig.colorbar(
-        cbar, ax=ax, cax=cax,
+        sm, ax=ax, cax=cax,
         orientation='vertical',
         ticks=[x+0.5 for x in bounds], # set labels in the middle of class
         label='Layers',
     )
     ax_cbar.ax.tick_params(size=0)
-    ax_cbar.set_ticklabels(['{:.0f}'.format(x) for x in bounds])
+    
+    _labels = copy(labels)
+    if _labels is None:
+        _labels = ['{:.0f}'.format(x) for x in bounds]
+    if len(_labels) != len(bounds):
+        _labels = _labels + ['none']
+    ax_cbar.set_ticklabels(_labels)
+    ax_cbar.ax.invert_yaxis()
     
     if file_out is not None:
         plt.savefig(file_out, dpi=300)
