edadc7626b69dfc55bca842029be3b875e506392
Adrien Manlay <a.manlay@brgm.fr>
2025-11-19 13:47:38 +0100

    build: update build config, editable option, changelog

--- src/gridmarthe/__version__.py
+++ src/gridmarthe/__version__.py
@@ -22,7 +22,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 _copyleft = """
     {program}  Copyright (C) {year}  {name_of_author}.
