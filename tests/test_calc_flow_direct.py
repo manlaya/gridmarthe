73362512bdfd1fe7a447bcf2f4d3d3888e87c56a
Shuaitao Wang <s.wang@brgm.fr>
2025-12-08 13:47:39 +0100

    Return xr.Dataset in calc_flow_directions

--- tests/test_calc_flow_direct.py
+++ tests/test_calc_flow_direct.py
@@ -2,6 +2,8 @@ from gridmarthe import calc_flow_directions
 import os
 import pandas as pd
 import numpy as np
+import xarray as xr
+import gridmarthe as gm
 
 DATA_PATH = './tests/data'
 
@@ -9,7 +11,14 @@ def test_calc_flow_direct():
     file_presence = os.path.join(os.path.abspath(DATA_PATH), "craie_npc.topog")
     file_topo = os.path.join(os.path.abspath(DATA_PATH), "craie_npc.topog")
 
-    calc_flow_directions(file_presence,file_topo)
+    ds = calc_flow_directions(file_presence,file_topo)
+    assert isinstance(ds, xr.Dataset)
+    
+    # write ds to grid data
+    # FIXME by default max_layer = 1
+    gm.write_marthe_grid(ds=ds, fileout=os.path.join(os.path.abspath(DATA_PATH), "test_write_dir_aval.d_ava"), varname='direct_aval')
+    ds_read = gm.load_marthe_grid(os.path.join(os.path.abspath(DATA_PATH), "test_write_dir_aval.d_ava"))
+    assert ds.equals(ds_read)
     
     # use gridmarthe to read full data ?
     dir_aval_python = pd.read_csv(f"{DATA_PATH}{os.sep}ddr_dir_aval.d_ava", skiprows=28, nrows=305, header=None, sep="\t")
@@ -17,8 +26,7 @@ def test_calc_flow_direct():
     
     topo_correct_python = pd.read_csv(f"{DATA_PATH}{os.sep}ddr_topo_corr.topop", skiprows=28, nrows=305, header=None, encoding="latin1", sep="\t")
     topo_correct_ref_fortran = pd.read_csv(f"{DATA_PATH}{os.sep}topo_corr_craie_npc_ref.topop", skiprows=28, nrows=305, header=None, encoding="latin1", sep="\t")
-        
-
+    
     assert np.allclose(dir_aval_python.values, dir_aval_ref_fortran.values, rtol=1e-05, atol=1e-08, equal_nan=True)
     assert np.allclose(topo_correct_python.values, topo_correct_ref_fortran.values, rtol=1e-05, atol=1e-08, equal_nan=True)
     
